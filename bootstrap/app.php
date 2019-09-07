4cbee12cb75ffc1e585d374a625a9b2972474dec
MilesChou <jangconan@gmail.com>
2019-09-07 22:23:59 +0800

    Implement google login

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,8 @@
 <?php
 
+use Dotenv\Dotenv;
 use Illuminate\Container\Container;
+use Illuminate\Support\Str;
 use OpenIDConnect\Client;
 use OpenIDConnect\Metadata\ClientMetadata;
 use OpenIDConnect\Metadata\ProviderMetadata;
@@ -10,9 +12,13 @@ use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+session_start();
+
+Dotenv::create(__DIR__ . '/../')->load();
+
 $container = new Container();
 
-$container->singleton(Client::class, function() {
+$container->singleton(Client::class, function () {
     $openIdConnectConfig = require __DIR__ . '/../config.php';
 
     $provider = new ProviderMetadata(
@@ -21,16 +27,48 @@ $container->singleton(Client::class, function() {
     );
 
     return new Client($provider, new ClientMetadata([
-        'client_id' => '',
-        'client_secret' => '',
-        'redirect_uri' => '',
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect_uri' => env('GOOGLE_REDIRECT_URI'),
     ]));
 });
 
 $app = AppFactory::create(null, $container);
 
-$app->get('/', function (RequestInterface $request, ResponseInterface $response) use ($container) {
+$app->get('/', function (RequestInterface $request, ResponseInterface $response) {
     $response->getBody()->write('Hi');
+
+    return $response;
+});
+
+$app->get('/login', function (RequestInterface $request, ResponseInterface $response) use ($container) {
+    /** @var Client $client */
+    $client = $container->make(Client::class);
+
+    $state = Str::random();
+
+    $_SESSION['state'] = $state;
+
+    $client->authorize([
+        'response_type' => 'code',
+        'scope' => 'openid profile email',
+        'state' => $state,
+    ]);
+});
+
+$app->get('/callback', function (RequestInterface $request, ResponseInterface $response) use ($container) {
+    /** @var Client $client */
+    $client = $container->make(Client::class);
+
+    parse_str($request->getUri()->getQuery(), $query);
+
+    $token = $client->handleOpenIDConnectCallback($query, [
+        'state' => $_SESSION['state'],
+    ]);
+
+    dump($token->jsonSerialize());
+
+    dump($token->idTokenClaims()->all());
 
     return $response;
 });
